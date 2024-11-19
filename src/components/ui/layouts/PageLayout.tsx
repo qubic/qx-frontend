a6717e8a8b55f2adee9e5a4e05085a00054677d6
alexmf91 <alexmunoz91@gmail.com>
2024-11-19 23:55:04 +0100

    feat: add PageLayout component for structured page layout and update index exports

---
+++ src/components/ui/layouts/PageLayout.tsx
@@ -0,0 +1,19 @@
+import { Alert } from '@app/components/ui'
+import { clsxTwMerge } from '@app/utils'
+
+type Props = {
+  title?: string
+  error?: string
+  children: React.ReactNode
+  className?: string
+}
+
+export default function PageLayout({ title, error, children, className }: Props) {
+  return (
+    <div className={clsxTwMerge('grid place-items-center gap-20 py-32', className)}>
+      {title && <h1 className="my-4 text-4xl font-bold">{title}</h1>}
+      {error && <Alert variant="error">{error}</Alert>}
+      {children}
+    </div>
+  )
+}
--- src/components/ui/layouts/index.ts
+++ src/components/ui/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as AppLayout } from './AppLayout'
 export { default as Footer } from './Footer'
-export { default as Header } from './Header'
+export { default as Header } from './Header/Header'
+export { default as PageLayout } from './PageLayout'
