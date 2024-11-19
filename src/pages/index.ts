ac4e5e240f9616dbd60cc9ee2ed5f0b6e1f6edd0
alexmf91 <alexmunoz91@gmail.com>
2024-11-20 00:00:55 +0100

    feat: add lazy-loaded draft Trades and Transactions pages with corresponding exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,8 @@
+import { lazy } from 'react'
+
 export { default as Error404Page } from './404/Error404Page'
 export { default as HomePage } from './home/HomePage'
+
+export const AssetsPageLazy = lazy(() => import('./assets/AssetsPage'))
+export const TradesPageLazy = lazy(() => import('./trades/TradesPage'))
+export const TransactionsPageLazy = lazy(() => import('./transactions/TransactionsPage'))
