5467d7f5dcecda32756fd607fcfd9741ab75cf71
alexmf91 <alexmunoz91@gmail.com>
2024-11-20 00:01:38 +0100

    feat: update BASE_URL to include API versioning for qxApi

--- src/store/apis/qx/qx.api.ts
+++ src/store/apis/qx/qx.api.ts
@@ -2,7 +2,7 @@ import { envConfig } from '@app/configs'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { Asset } from './qx.types'
 
-const BASE_URL = envConfig.QX_API_URL
+const BASE_URL = `${envConfig.QX_API_URL}/v1/qx`
 
 export const qxApi = createApi({
   reducerPath: 'qxApi',
