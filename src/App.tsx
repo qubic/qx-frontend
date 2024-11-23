632bb0b220e595d0040fed3006baa091bae59f15
alexmf91 <alexmunoz91@gmail.com>
2024-11-23 11:31:58 +0100

    feat: add withHelmet higher-order component and integrate react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'react-hot-toast'
 import { I18nextProvider } from 'react-i18next'
 import { Provider as ReduxProvider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 
-import ModalManager from './components/modals/ModalManager'
+import { ModalManager } from './components/modals'
 import i18n from './configs/i18n'
 import { WalletConnectProvider } from './contexts'
 import { router } from './router'
@@ -11,14 +12,16 @@ import { store } from './store'
 
 export default function App() {
   return (
-    <I18nextProvider i18n={i18n}>
-      <ReduxProvider store={store}>
-        <Toaster position="bottom-right" />
-        <WalletConnectProvider>
-          <RouterProvider router={router} />
-          <ModalManager />
-        </WalletConnectProvider>
-      </ReduxProvider>
-    </I18nextProvider>
+    <HelmetProvider>
+      <I18nextProvider i18n={i18n}>
+        <ReduxProvider store={store}>
+          <Toaster position="bottom-right" />
+          <WalletConnectProvider>
+            <RouterProvider router={router} />
+            <ModalManager />
+          </WalletConnectProvider>
+        </ReduxProvider>
+      </I18nextProvider>
+    </HelmetProvider>
   )
 }
