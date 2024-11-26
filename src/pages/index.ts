29bf99ee6657d3738753cb3df0b59e3e4c8be3f1
alexmf91 <alexmunoz91@gmail.com>
2024-11-26 21:29:46 +0100

    feat: add lazy loading for AssetPage and EntityPage in index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,5 +4,7 @@ export { default as Error404Page } from './404/Error404Page'
 export { default as HomePage } from './home/HomePage'
 
 export const AssetsPageLazy = lazy(() => import('./assets/AssetsPage'))
+export const AssetPageLazy = lazy(() => import('./assets/AssetPage'))
 export const TradesPageLazy = lazy(() => import('./trades/TradesPage'))
 export const TransactionsPageLazy = lazy(() => import('./transactions/TransactionsPage'))
+export const EntityPageLazy = lazy(() => import('./entities/EntityPage'))
