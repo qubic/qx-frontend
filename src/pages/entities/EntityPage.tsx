db6cbfe14bc3b16941e3003fc914c032e2bba2ce
alexmf91 <alexmunoz91@gmail.com>
2024-11-27 21:04:48 +0100

    feat: refactor EntityPage to streamline data fetching and add ExplorerLink component

--- src/pages/entities/EntityPage.tsx
+++ src/pages/entities/EntityPage.tsx
@@ -4,68 +4,63 @@ import { useParams } from 'react-router-dom'
 import { withHelmet } from '@app/components/hocs'
 import { TradesTable, TransfersTable } from '@app/components/tables'
 import { PageLayout } from '@app/components/ui/layouts'
+import { ExplorerLink } from '@app/components/ui/links'
 import {
   useGetEntityAskOrdersQuery,
   useGetEntityBidOrdersQuery,
   useGetEntityTradesQuery,
   useGetEntityTransfersQuery
 } from '@app/store/apis/qx'
+import { ExplorerLinkType } from '@app/types/enums'
 import { EntityOrdersTable } from './components'
 
 function EntityPage() {
   const { entity = '' } = useParams()
   const { t } = useTranslation()
-  const {
-    data: bidOrders,
-    isFetching: isBidOrdersLoading,
-    isError: isBidOrdersError
-  } = useGetEntityBidOrdersQuery({ entity }, { skip: !entity })
-  const {
-    data: askOrders,
-    isFetching: isAskOrdersLoading,
-    isError: isAskOrdersError
-  } = useGetEntityAskOrdersQuery({ entity }, { skip: !entity })
-  const {
-    data: trades,
-    isFetching: isTradesLoading,
-    isError: isTradesError
-  } = useGetEntityTradesQuery({ entity }, { skip: !entity })
-  const {
-    data: transfers,
-    isFetching: isTransfersLoading,
-    isError: isTransfersError
-  } = useGetEntityTransfersQuery({ entity }, { skip: !entity })
+
+  const bidOrders = useGetEntityBidOrdersQuery({ entity }, { skip: !entity })
+  const askOrders = useGetEntityAskOrdersQuery({ entity }, { skip: !entity })
+  const trades = useGetEntityTradesQuery({ entity }, { skip: !entity })
+  const transfers = useGetEntityTransfersQuery({ entity }, { skip: !entity })
 
   return (
     <PageLayout title="Entity Info">
+      <ExplorerLink
+        type={ExplorerLinkType.ADDRESS}
+        value={entity}
+        className="!text-base"
+        copy
+        showTooltip
+        tooltipContent={t('global.check_on_explorer')}
+      />
       <section className="grid w-[85vw] max-w-2xl gap-24 md:grid-cols-2">
         <section className="flex flex-col gap-24">
           <h2 className="text-center text-xl font-bold">{t('entity_page.open_ask_orders')}</h2>
           <EntityOrdersTable
-            entityOrders={bidOrders}
-            isLoading={isBidOrdersLoading}
-            hasError={isBidOrdersError}
+            entityOrders={bidOrders.data}
+            isLoading={bidOrders.isFetching}
+            hasError={bidOrders.isError}
           />
         </section>
         <section className="flex flex-col gap-24">
           <h2 className="text-center text-xl font-bold">{t('entity_page.open_bid_orders')}</h2>
           <EntityOrdersTable
-            entityOrders={askOrders}
-            isLoading={isAskOrdersLoading}
-            hasError={isAskOrdersError}
+            entityOrders={askOrders.data}
+            isLoading={askOrders.isFetching}
+            hasError={askOrders.isError}
           />
         </section>
       </section>
       <section className="grid gap-24">
         <h2 className="text-center text-xl font-bold">{t('global.trades')}</h2>
-        <TradesTable trades={trades} isLoading={isTradesLoading} hasError={isTradesError} />
+        <TradesTable trades={trades.data} isLoading={trades.isFetching} hasError={trades.isError} />
       </section>
       <section className="grid gap-24">
         <h2 className="text-center text-xl font-bold">{t('global.asset_transfers')}</h2>
         <TransfersTable
-          transfers={transfers}
-          isLoading={isTransfersLoading}
-          hasError={isTransfersError}
+          transfers={transfers.data}
+          isLoading={transfers.isFetching}
+          hasError={transfers.isError}
         />
       </section>
     </PageLayout>
