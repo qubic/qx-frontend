b8e33e0fb8b8777337cf1b3a0151e25dfc1e536a
alexmf91 <alexmunoz91@gmail.com>
2024-11-30 13:13:05 +0100

    feat: update logo export and add new QubicQxWhiteLogo SVG

--- src/assets/icons/logo/index.ts
+++ src/assets/icons/logo/index.ts
@@ -1,4 +1,4 @@
 export { default as QubicLogoShort } from './logo-text-short.svg?react'
 export { default as QubicIcon } from './logo.svg?react'
 export { default as QubicConnectLogo } from './qubic-connect.svg?react'
-export { default as QubicQxInfoWhiteLogo } from './qx-info-logo.svg?react'
+export { default as QubicQxWhiteLogo } from './qx-logo.svg?react'
