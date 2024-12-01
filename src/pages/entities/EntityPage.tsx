2340e42517f62949fc997a73700952f3c0f46854
alexmf91 <alexmunoz91@gmail.com>
2024-12-01 11:45:15 +0100

    feat: enhance EntityPage layout with ExplorerLink and address display

--- src/pages/entities/EntityPage.tsx
+++ src/pages/entities/EntityPage.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 
+import { ExplorerIcon } from '@app/assets/icons'
 import { withHelmet } from '@app/components/hocs'
 import { TradesTable, TransfersTable } from '@app/components/tables'
 import { PageLayout } from '@app/components/ui/layouts'
@@ -25,14 +26,19 @@ function EntityPage() {
 
   return (
     <PageLayout title="Entity Info">
-      <ExplorerLink
-        type={ExplorerLinkType.ADDRESS}
-        value={entity}
-        className="!text-base"
-        copy
-        showTooltip
-        tooltipContent={t('global.check_on_explorer')}
-      />
+      <section className="grid w-[85vw] max-w-2xl text-sm">
+        <div className="flex gap-8 border-y-1 border-primary-60 py-12">
+          <p className="text-slate-500">{t('global.address')}</p>
+          <p className="break-all text-xxs xs:text-xs sm:text-sm">{entity}</p>
+          <ExplorerLink
+            type={ExplorerLinkType.ADDRESS}
+            value={entity}
+            label={<ExplorerIcon className="size-20 text-primary-25" />}
+            showTooltip
+            tooltipContent={t('global.check_on_explorer')}
+          />
+        </div>
+      </section>
       <section className="grid w-[85vw] max-w-2xl gap-24 md:grid-cols-2">
         <section className="flex flex-col gap-24">
           <h2 className="text-center text-xl font-bold">{t('entity_page.open_ask_orders')}</h2>
