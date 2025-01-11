0e5e16599359d69d34404a1cfae8a18d15064c1a
alexmf91 <alexmunoz91@gmail.com>
2025-01-11 08:28:10 +0100

    feat: add rollup-plugin-visualizer for build analysis and lazy load error images

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      visualizer({
+        filename: './dist/stats.html',
+        open: true
+      })
+    ],
     define: {
       __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     },
@@ -14,6 +22,17 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@app': path.resolve(__dirname, 'src')
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            lottie: ['lottie-react'],
+            qubicLib: ['@qubic-lib/qubic-ts-library']
+          }
+        }
       }
     }
   }
