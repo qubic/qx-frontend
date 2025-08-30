1c05336911da9d39235b7f206011031fb4a3fe7f
mio <180114449+qubicmio@users.noreply.github.com>
2025-08-30 09:28:52 +0000

    feat: [#46] Add calls to new smart contract and token trade endpoints

--- src/pages/trades/TradesPage.tsx
+++ src/pages/trades/TradesPage.tsx
@@ -1,39 +1,36 @@
-import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { withHelmet } from '@app/components/hocs'
 import { TradesTable } from '@app/components/tables'
 import { PageLayout } from '@app/components/ui/layouts'
-import { type Trade, useGetTradesQuery } from '@app/store/apis/qx'
-import { ASSETS_ISSUER_ADDRESS } from '@app/utils/qubic'
+import { useGetSmartContractTradesQuery, useGetTokenTradesQuery } from '@app/store/apis/qx'
 
 function TradesPage() {
   const { t } = useTranslation()
-  const { data: trades = [], isFetching, isError } = useGetTradesQuery()
-
-  // TODO implement two calls: one for smart contracts and one for tokens because there are only few smart contract trades
-  const { smartContractShares, tokens } = useMemo(
-    () =>
-      trades.reduce(
-        (acc: { smartContractShares: Trade[]; tokens: Trade[] }, trade) => {
-          const key = trade.issuer === ASSETS_ISSUER_ADDRESS ? 'smartContractShares' : 'tokens'
-          acc[key].push(trade)
-          return acc
-        },
-        { smartContractShares: [], tokens: [] }
-      ),
-    [trades]
-  )
+  const {
+    data: scTrades = [],
+    isFetching: isScTradesFetching,
+    isError: isScTradesError
+  } = useGetSmartContractTradesQuery()
+  const {
+    data: tokenTrades = [],
+    isFetching: isTokenTradesFetching,
+    isError: isTokenTradesError
+  } = useGetTokenTradesQuery()
 
   return (
     <PageLayout title={t('trades_page.qx_trades')}>
       <section className="grid gap-24">
         <h2 className="text-center text-xl font-semibold">{t('global.smart_contract_shares')}</h2>
-        <TradesTable trades={smartContractShares} isLoading={isFetching} hasError={isError} />
+        <TradesTable trades={scTrades} isLoading={isScTradesFetching} hasError={isScTradesError} />
       </section>
       <section className="grid gap-24">
         <h2 className="text-center text-xl font-semibold">{t('global.tokens')}</h2>
-        <TradesTable trades={tokens} isLoading={isFetching} hasError={isError} />
+        <TradesTable
+          trades={tokenTrades}
+          isLoading={isTokenTradesFetching}
+          hasError={isTokenTradesError}
+        />
       </section>
     </PageLayout>
   )
